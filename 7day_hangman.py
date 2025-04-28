c85bb8077e73adf7376c4ff33eb61d043a28675e
Niyihoraho <heroicrwand@gmail.com>
2025-04-29 01:08:31 +0200

    7day hangman

--- 7day_hangman.py
+++ 7day_hangman.py
@@ -1,17 +1,106 @@
 import random
 
-#Step 1 
-
 word_list = ["aardvark", "baboon", "camel"]
-# rand_num = random.randint(0,2)
-# word = word_list[rand_num]
-word=random.choice(word_list)
-print(word)
-length=len(word)
-display=["_"for _ in range(length)]
-print(display)
-guess= input(str("enter guessing letter: ")).lower()
-for index in range(length):
-    if guess == word[index]:
-            display[index]=guess
-print(display)
+word = random.choice(word_list)
+
+logo = ''' 
+ _                                             
+| |                                            
+| |__   __ _ _ __   __ _ _ __ ___   __ _ _ __  
+| '_ \ / _` | '_ \ / _` | '_ ` _ \ / _` | '_ \ 
+| | | | (_| | | | | (_| | | | | | | (_| | | | |
+|_| |_|\__,_|_| |_|\__, |_| |_| |_|\__,_|_| |_|
+                    __/ |                      
+                   |___/    '''
+
+
+print(logo)
+stages = ['''
+  +---+
+  |   |
+  O   |
+ /|\  |
+ / \  |
+      |
+=========
+''', '''
+  +---+
+  |   |
+  O   |
+ /|\  |
+ /    |
+      |
+=========
+''', '''
+  +---+
+  |   |
+  O   |
+ /|\  |
+      |
+      |
+=========
+''', '''
+  +---+
+  |   |
+  O   |
+ /|   |
+      |
+      |
+=========''', '''
+  +---+
+  |   |
+  O   |
+  |   |
+      |
+      |
+=========
+''', '''
+  +---+
+  |   |
+  O   |
+      |
+      |
+      |
+=========
+''', '''
+  +---+
+  |   |
+      |
+      |
+      |
+      |
+=========
+''']
+
+stages_reverse = stages[::-1]
+
+display = []
+word_length = len(word)
+for letter in word:
+    display += "_"
+print(stages[6])
+print(f"{' '.join(display)}")
+
+lives = 6
+
+end_of_game = False
+while not end_of_game:
+    guess = input("Guess the letter: ").lower()
+    
+    for i in range(word_length):
+        if guess == word[i]:
+            display[i] = guess
+    print(f"{' '.join(display)}")
+    
+    if "_" not in display:
+        end_of_game = True
+        print("You win")
+        
+    if guess not in word:
+        lives = lives - 1
+        
+        if lives == 0:
+            end_of_game = True
+            print("You lose the game!!")
+        
+        print(stages[lives]) 
