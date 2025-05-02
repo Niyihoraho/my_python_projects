aaf7aecc8f52a6e1ab1ed4e8ea9066f199758b58
Niyihoraho <heroicrwand@gmail.com>
2025-05-02 20:09:00 +0200

    9day work 1

---
+++ 9day_work-1.py
@@ -0,0 +1,17 @@
+student_scores = {}
+end = True
+
+while end:
+    name = input("Enter your name: ")
+    marks = int(input("Enter your marks: "))
+
+    student_scores[name] = marks
+
+    for key in student_scores:
+        value = student_scores[key]
+        print(f"{key}: {value}")
+
+    exitt = input("Do you want to exit? (yes/no): ")
+    if exitt.lower() == 'yes':
+        end = False
+        print("You exited.")
