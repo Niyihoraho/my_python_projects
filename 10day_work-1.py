57be3ac8a06f462e6e6c586b99b709018420fa6f
Niyihoraho <heroicrwand@gmail.com>
2025-05-05 22:40:55 +0200

    10day work 1

---
+++ 10day_work-1.py
@@ -0,0 +1,34 @@
+import os
+def clear():
+    os.system('cls' if os.name == 'nt' else 'clear')
+clear()
+def is_leap(year):
+  if year % 4 == 0:
+    if year % 100 == 0:
+      if year % 400 == 0:
+        return "Leap year"
+      else:
+        return "Not leap year"
+    else:
+      return "Leap year"
+  else:
+    return "Not leap year"
+
+result= is_leap(2023)
+print(result)  
+
+def days_in_month(year_test,month_test):
+  month_days = [31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31]
+  value=month_test-1
+  if month == 2 and is_leap(year_test):
+        return f"Leap Year- Month: 2 days of Month : 29"       
+  else:
+    return f"Not Leap Year! Month: {month_test} days of Month: {month_days[value]}"
+  
+    
+
+year = int(input("Enter a year: ")) 
+month = int(input("Enter a month: ")) 
+days = days_in_month(year, month)
+print(days)
+
